818fab52a249c26fb9b58696869a65fc7fd503b7
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-12-19 17:56:04 +0200

    Finish first Task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profile/Profile';
+import user from './assets/user.json'
+import PropTypes from 'prop-types';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />
   </React.StrictMode>
 );
+
+
